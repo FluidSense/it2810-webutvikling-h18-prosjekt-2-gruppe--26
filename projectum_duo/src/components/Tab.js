863e5ca41f8d4e8788d419021bc2d6d54f989d0e
PeterRydberg <peter.rydberg@hotmail.no>
2018-09-04 12:39:15 +0200

    Added a simple tab component

---
+++ projectum_duo/src/components/Tab.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const style = {
+  backgroundcolor: "#666666",
+  overflow: "hidden",
+  border: "1px",
+  bordercolor: "#a3a3a3"
+};
+
+class Tab extends React.Component{
+  render(){
+    return(
+      <div style={style}></div>
+    );
+  };
+};
+
+export default Tab;
