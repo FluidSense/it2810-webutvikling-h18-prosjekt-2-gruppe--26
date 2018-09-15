b85558cad5ddd14cb6f2d463af8b12da575b7e39
Johannes Kvamme <johanneskv96@gmail.com>
2018-09-15 13:04:04 +0200

    Revert "#2: Working selection and adding tabs"
    This reverts commit 5d027984ebeccf441411b54b0a7e305745889135.

--- projectum_duo/src/App.js
+++ projectum_duo/src/App.js
@@ -8,12 +8,17 @@ import TabButton from './components/TabButton';
 
 class App extends Component {
 
-
+  tabs = [
+    <TabButton tabName="Poop"></TabButton>,
+    <TabButton tabName="Schnoop"></TabButton>,
+    <TabButton tabName="verylongtabname"></TabButton>
+  ]
 
   render() {
       return (
           <div className="App">
             <TabSection tabs={this.tabs} />
+
             <ApiCall apiUrl="poems.json"/>
           </div>
       );
--- projectum_duo/src/components/TabSection.js
+++ projectum_duo/src/components/TabSection.js
@@ -9,50 +9,11 @@ const style = {
 };
 
 class TabSection extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      selectedTab: null,
-    }
-  }
-
-  tabTexts = [1];
-
-  handleClick(e) {
-    let id = parseInt(e.target.id,10);
-    this.setState({
-      selectedTab: id,
-    });
-  }
-
-  handleAddTab() {
-    this.tabTexts.push(this.tabTexts.length+1);
-    this.forceUpdate();
-  }
-
-  createTabs () {
-    let tabs = []
-    for(const textNum in this.tabTexts) {
-        let thisId = this.tabTexts[textNum];
-        tabs.push(
-          <TabButton
-            id={thisId}
-            key={thisId}
-            tabName={"Tab " + this.tabTexts[textNum]}
-            selected={this.state.selectedTab === thisId}
-            clickFunction={this.handleClick.bind(this)}
-          />
-        );
-    }
-    tabs.push(<TabButton tabName="+" id="addTab" clickFunction={this.handleAddTab.bind(this)}/>)
-
-    return tabs;
-  }
-
   render(){
     return(
       <div style={style}>
-        {this.createTabs()}
+        {this.props.tabs}
+        <TabButton tabName="+"></TabButton>
       </div>
     );
   };
